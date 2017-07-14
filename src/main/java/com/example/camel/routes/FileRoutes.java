93ef693c1137ef9d799f1b73b803f2f5dd439bcf
Igor Ivaniuk <igor.ivaniuk@zooplus.com>
2017-07-14 16:17:18 +0200

    Added file writer route

--- src/main/java/com/example/camel/routes/FileRoutes.java
+++ src/main/java/com/example/camel/routes/FileRoutes.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
  * Created by igori on 14.07.2017.
  */
 @Component
-public class FileConsumingRoute extends RouteBuilder {
+public class FileRoutes extends RouteBuilder {
 
     // You can autowire your beans or processors in the route definition
     @Autowired
@@ -26,14 +26,27 @@ public class FileConsumingRoute extends RouteBuilder {
                 + "&move=.done/${file:onlyname}.${date:now:yyyMMdd_HHmmss}" // move input file to this folder AFTER successful processing, and append timestamp to the name
                 + "&moveFailed=../.error" // move input file to this folder if error happened. If there is no error folder, file will stay in input and Camel will try to process it again
                 + "&include=.*.xml" // Regex include filter
+                // File endpoint has many other options, including retry policies, duplicates detection etc. Check this: http://camel.apache.org/file2.html
                 ;
 
-        // File endpoint has many other options, including retry policies, duplicates detection etc. Check this: http://camel.apache.org/file2.html
+        String outputFileEndpointURI = "file://{{fileRoute.dest}}" // Output folder
+                + "?fileName=${header.OriginalFileName}.processed.${date:now:yyyMMdd_HHmmss}" // Output filename
+                ;
+
+        // File reader route
         from(inputFileEndpointURI)
+                .routeId("FileReader")
                 .setHeader("OriginalFileName", ExpressionBuilder.headerExpression("CamelFileName")) // Camel sets a lot of headers that travel together with Exchange. We populate custom header to reuse it in beans
                 .process(stdoutProcessor)
                 .bean(ParsedXmlObjectBean.class)
                 .to("stream:out") // One more log after author replacement
+                .to("direct://fileWriterRoute") //One oute can have multiple outputs, they will be called in seqience by default
+        // direct endpoint directly calls other route within same Camel context
+        // seda endpoint creates an in-memory queue for non-blocking calls, also within one Camel Context
         ;
+
+        // File writer route
+        from("direct://fileWriterRoute")
+                .to(outputFileEndpointURI);
     }
 }
